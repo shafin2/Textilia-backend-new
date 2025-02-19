85ec82a43f63ffc453d05b764f36a5caba33bc4c
Shafin zaman <shafinzaman42@gmail.com>
2025-02-19 18:22:15 +0500

    Fix cors

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const { REMOTE_CLIENT_URL, PORT } = require("./config/dotenv");
 const app = express();
 
 var corsOptions = {
-	origin: REMOTE_CLIENT_URL,
+	origin: REMOTE_CLIENT_URL || "http://localhost:3000",
 	optionsSuccessStatus: 200,
 	credentials: true,
 };
